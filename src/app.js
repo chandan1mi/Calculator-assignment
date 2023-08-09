479bf42f519ae8c55d8bb3f2c86c3d6d5d365b29
cafrmen <cafrmen@gmail.com>
2023-08-08 22:21:38 -0600

    delete some useless stuff

--- src/app.js
+++ src/app.js
@@ -5,13 +5,11 @@ const clearBtn = document.querySelector('#clear');
 const deleteBtn = document.querySelector('#erase');
 const resultBtn = document.querySelector('#equals');
 
-// operation functions
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-//calculator display and operation for future functionalities
 let operator = '';
 let inputArray = [];
 
@@ -29,20 +27,19 @@ function eraseButton() {
 function operate(button) {
     let value = button.textContent;
     inputArray.push(value);
-    //clear and delete function
+
     if (value === 'AC') {
         clearButton();
     } else if (value === '↼') {
        eraseButton();
     }
-    // missing separate numbers and operator
-    // button.id.match('num')
+
     let theArray = inputArray.join('');
     inputDisplay.textContent = theArray;
 
     console.log('input array:' + inputArray)
     console.log('operator:' + operator);
-    console.log('the array:' + theArray); // need to separate values
+    console.log('the array:' + theArray);
 }
 
 buttons.forEach(button => button.addEventListener('click', () => operate(button)));
